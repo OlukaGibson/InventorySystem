cd538ea4b0bc23b4d05d66c7f1018aebd67d8b03
OlukaGibson <gibsonoluka7@gmail.com>
2023-09-14 08:38:39 +0300

    api edits

--- apiapp/views.py
+++ apiapp/views.py
@@ -2,8 +2,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import JsonResponse
 from .models import FirmwareUpdate
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 
 class UpdateSensorDataView(APIView):
     def get(self, request, device_id, *args, **kwargs):
@@ -50,61 +48,6 @@ class UpdateSensorDataView(APIView):
                 },
                 status=200
                 )
-
-
-@login_required
-def display_firmware_update(request):
-    firmware_updates = FirmwareUpdate.objects.all().order_by('-device_id')
-
-    # Check if a grouping parameter is provided in the URL
-    group_by = request.GET.get('group_by')
-
-    if group_by == 'version':
-        firmware_updates = firmware_updates.order_by('version')
-    elif group_by == 'batteryValue':
-        firmware_updates = firmware_updates.order_by('batteryValue')
-    elif group_by == 'spvValue':
-        firmware_updates = firmware_updates.order_by('spvValue')
-
-    context = {
-        'firmware_updates': firmware_updates,
-        'group_by': group_by
-        }
-    
-    return render(request, 'appapi/firmware_update.html', context)
-
-@login_required
-def update_selected_values(request):
-    if request.method == 'POST':
-        selected_ids = request.POST.getlist('selected_updates')
-        field_to_edit = request.POST.get('field_to_edit')
-        new_value = request.POST.get(field_to_edit)
-
-        if selected_ids and field_to_edit in ('spvValue', 'batteryValue'):
-            field_to_update = {field_to_edit: new_value}
-            FirmwareUpdate.objects.filter(id__in=selected_ids).update(**field_to_update)
-
-            # Respond with a success message (you can customize this JSON response)
-            response_data = {'message': f'{field_to_edit} updated successfully.'}
-            return JsonResponse(response_data)
-
-    # If the form submission is invalid or no updates were made, respond with an error message
-    response_data = {'error': 'Invalid form submission or no updates were made.'}
-    return JsonResponse(response_data, status=400)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         # return Response(
         #         {
