c233220ea9f555de83974232f55b40146ae812b9
OlukaGibson <gibsonoluka7@gmail.com>
2023-09-23 16:00:49 +0300

    on the low.....

--- apiapp/models.py
+++ apiapp/models.py
@@ -2,17 +2,17 @@
 from django.db import models
 
 class Device(models.Model):
-    device_name = models.CharField(max_length=50)
-    channel_id = models.CharField(max_length=50)
-    created_at = models.DateTimeField()
+    device_name = models.CharField(max_length=50,default='Device Name', null=True)
+    channel_id = models.CharField(max_length=50,default='Channel ID', null=True)
+    created_at = models.DateTimeField(auto_created=True, null=True)
 
     def __str__(self):  
         return self.device_name
 
 class Firmware(models.Model):
-    firmware_version = models.CharField(max_length=20)
-    firmware_version_file = models.FileField(upload_to='firmware/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    firmware_version = models.CharField(max_length=20, default='Firmware Version', null=True)
+    firmware_version_file = models.FileField(upload_to='firmware/', null=True)
+    uploaded_at = models.DateTimeField(auto_created=True)
 
     def __str__(self):
         return self.firmware_version
@@ -20,24 +20,35 @@ class Firmware(models.Model):
 class FirmwareUpdate(models.Model):
     device_name = models.ForeignKey(Device, on_delete=models.CASCADE)
     firmware = models.ForeignKey(Firmware, on_delete=models.CASCADE)
-    fileDownload = models.IntegerField()
-    spvValue = models.PositiveIntegerField()
-    syncState = models.IntegerField()
-    confrigDownload = models.IntegerField()
+    fileDownload = models.IntegerField(default=0, null=True)
+    spvValue = models.PositiveIntegerField(default=0, null=True)
+    syncState = models.IntegerField(default=0, null=True)
+    confrigDownload = models.IntegerField(default=0, null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.spvValue
-    
+
+class FirmwareUpdateStats(models.Model):
+    device_name = models.ForeignKey(Device, on_delete=models.CASCADE)
+    firmware = models.ForeignKey(Firmware, on_delete=models.CASCADE)
+    fileDownload = models.IntegerField(default=0, null=True)
+    spvValue = models.PositiveIntegerField(default=0, null=True)
+    syncState = models.IntegerField(default=0, null=True)
+    confrigDownload = models.IntegerField(default=0, null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.spvValue    
 class FirmwareUpdateHistory(models.Model):
     device_name = models.ForeignKey(Device, on_delete=models.CASCADE)
     firmware = models.ForeignKey(Firmware, on_delete=models.CASCADE)
-    fileDownload = models.IntegerField()
-    spvValue = models.PositiveIntegerField()
-    syncState = models.IntegerField()
-    confrigDownload = models.IntegerField()
-    uploaded_at = models.DateTimeField(auto_now_add=False)
-    history_date = models.DateTimeField(auto_now=True, null=True)
+    fileDownload = models.IntegerField(default=0, null=True)
+    spvValue = models.PositiveIntegerField(default=0, null=True)
+    syncState = models.IntegerField(default=0, null=True)
+    confrigDownload = models.IntegerField(default=0, null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    history_date = models.DateTimeField(auto_created=True, null=True)
 
     def __str__(self):
         return self.spvValue
