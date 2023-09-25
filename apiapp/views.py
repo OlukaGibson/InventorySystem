ba355ccaeafd40f25c5a435ac134d0e187a64d16
OlukaGibson <gibsonoluka7@gmail.com>
2023-09-25 10:37:41 +0300

    Filedownload done

--- apiapp/views.py
+++ apiapp/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import FileResponse
-from django.http import JsonResponse  # Import JsonResponse
+from django.http import JsonResponse
 
 from .models import FirmwareUpdate, Device, Firmware
 
@@ -56,15 +56,84 @@ class UpdateSensorDataView(APIView):
         # Set the Content-Disposition header to specify the filename for download
         file_response['Content-Disposition'] = f'attachment; filename="{firmware_update.firmware.firmware_version_file.name}"'
 
-        # Construct the JSON response with both data and the file URL
+        # Construct the JSON response with the JSON data
         response_data = {
             'message': 'SPV value updated successfully',
             'data': data,
-            'file_url': file_response.url,  # Include the file URL in the response
         }
 
-        # Return the JSON response
-        return JsonResponse(response_data)
+        # Return both the JSON data and the file response
+        response = JsonResponse(response_data, headers={'X-Accel-Redirect': file_response.url})
+        return response
+
+
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from django.http import FileResponse
+# from django.http import JsonResponse  # Import JsonResponse
+
+# from .models import FirmwareUpdate, Device, Firmware
+
+# class UpdateSensorDataView(APIView):
+#     def get(self, request, device_id, *args, **kwargs):
+#         try:
+#             spvValue = int(request.query_params.get('spvValue'))
+#         except (ValueError, TypeError):
+#             return Response(
+#                 {
+#                     'error': 'Invalid input data'
+#                 },
+#                 status=400
+#             )
+
+#         try:
+#             device = Device.objects.get(pk=device_id)
+#         except Device.DoesNotExist:
+#             return Response(
+#                 {
+#                     'error': 'Device not found'
+#                 },
+#                 status=404
+#             )
+
+#         # Get the latest firmware update for the device
+#         try:
+#             firmware_update = FirmwareUpdate.objects.filter(device_name=device).latest('uploaded_at')
+#         except FirmwareUpdate.DoesNotExist:
+#             return Response(
+#                 {
+#                     'error': 'Firmware update not found for this device'
+#                 },
+#                 status=404
+#             )
+
+#         # Update the spvValue
+#         firmware_update.spvValue = spvValue
+#         firmware_update.save()
+
+#         # Prepare the response data as a JSON dictionary
+#         data = {
+#             'device_id': device.id,
+#             'device_name': device.device_name,
+#             'channel_id': device.channel_id,
+#             'firmware_version': firmware_update.firmware.firmware_version,
+#         }
+
+#         # Create a file response for the firmware version file
+#         file_response = FileResponse(open(firmware_update.firmware.firmware_version_file.path, 'rb'))
+
+#         # Set the Content-Disposition header to specify the filename for download
+#         file_response['Content-Disposition'] = f'attachment; filename="{firmware_update.firmware.firmware_version_file.name}"'
+
+#         # Construct the JSON response with both data and the file URL
+#         response_data = {
+#             'message': 'SPV value updated successfully',
+#             'data': data,
+#             'file_url': file_response.url,  # Include the file URL in the response
+#         }
+
+#         # Return the JSON response
+#         return JsonResponse(response_data)
 
 # from rest_framework.views import APIView
 # from rest_framework.response import Response
