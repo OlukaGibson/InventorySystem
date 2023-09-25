681532dc1a9b275ab836bd773cc49ab56a1aa385
OlukaGibson <gibsonoluka7@gmail.com>
2023-09-25 13:57:03 +0300

    Filedownload done

--- apiapp/views.py
+++ apiapp/views.py
@@ -1,7 +1,6 @@
 # from rest_framework.views import APIView
+# from rest_framework.response import Response
 # from django.http import FileResponse
-# from django.http import JsonResponse
-
 # from .models import FirmwareUpdate, Device, Firmware
 
 # class UpdateSensorDataView(APIView):
@@ -9,7 +8,7 @@
 #         try:
 #             spvValue = int(request.query_params.get('spvValue'))
 #         except (ValueError, TypeError):
-#             return JsonResponse(
+#             return Response(
 #                 {
 #                     'error': 'Invalid input data'
 #                 },
@@ -19,7 +18,7 @@
 #         try:
 #             device = Device.objects.get(pk=device_id)
 #         except Device.DoesNotExist:
-#             return JsonResponse(
+#             return Response(
 #                 {
 #                     'error': 'Device not found'
 #                 },
@@ -30,7 +29,7 @@
 #         try:
 #             firmware_update = FirmwareUpdate.objects.filter(device_name=device).latest('uploaded_at')
 #         except FirmwareUpdate.DoesNotExist:
-#             return JsonResponse(
+#             return Response(
 #                 {
 #                     'error': 'Firmware update not found for this device'
 #                 },
@@ -41,7 +40,7 @@
 #         firmware_update.spvValue = spvValue
 #         firmware_update.save()
 
-#         # Prepare the response data as a JSON dictionary
+#         # Prepare the response data
 #         data = {
 #             'device_id': device.id,
 #             'device_name': device.device_name,
@@ -55,16 +54,8 @@
 #         # Set the Content-Disposition header to specify the filename for download
 #         file_response['Content-Disposition'] = f'attachment; filename="{firmware_update.firmware.firmware_version_file.name}"'
 
-#         # Construct the JSON response with the JSON data and add file streaming content
-#         response_data = {
-#             'message': 'SPV value updated successfully',
-#             'data': data,
-#             'file_streaming_content': file_response.streaming_content
-#         }
-
-#         # Return the JSON data along with the file streaming content
-#         response = JsonResponse(response_data)
-#         return response
+#         # Return the file response along with the data
+#         return file_response
 
 
 from rest_framework.views import APIView
@@ -107,21 +98,35 @@ class UpdateSensorDataView(APIView):
 
         # Update the spvValue
         firmware_update.spvValue = spvValue
-        firmware_update.save()
+        
 
-        # Prepare the response data
-        data = {
-            'device_id': device.id,
-            'device_name': device.device_name,
-            'channel_id': device.channel_id,
-            'firmware_version': firmware_update.firmware.firmware_version,
-        }
+        if firmware_update.fileDownload == 1:
+            firmware_update.fileDownload = 0
+            firmware_update.save()
+            # Prepare the response data
+            data = {
+                'device_name': device.device_name,
+                'channel_id': device.channel_id,
+                'firmware_version': firmware_update.firmware.firmware_version,
+                'fileDownload': firmware_update.fileDownload,
+                'spvValue': firmware_update.spvValue,
+                'syncState': firmware_update.syncState,
+                'confrigDownload': firmware_update.confrigDownload,
+            }
+            return Response(
+                {
+                    'message': 'SPV value updated successfully',
+                    'data': data,
+                },
+                status=200
+            )
+        else:
+            firmware_update.save()
+            # Create a file response for the firmware version file
+            file_response = FileResponse(open(firmware_update.firmware.firmware_version_file.path, 'rb'))
 
-        # Create a file response for the firmware version file
-        file_response = FileResponse(open(firmware_update.firmware.firmware_version_file.path, 'rb'))
+            # Set the Content-Disposition header to specify the filename for download
+            file_response['Content-Disposition'] = f'attachment; filename="{firmware_update.firmware.firmware_version_file.name}"'
 
-        # Set the Content-Disposition header to specify the filename for download
-        file_response['Content-Disposition'] = f'attachment; filename="{firmware_update.firmware.firmware_version_file.name}"'
-
-        # Return the file response along with the data
-        return file_response
+            # Return the file response along with the data
+            return file_response
