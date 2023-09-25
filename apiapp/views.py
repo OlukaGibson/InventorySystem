26fd43cdebfe828ef6f6359e60932d489e4afa3e
OlukaGibson <gibsonoluka7@gmail.com>
2023-09-25 09:28:33 +0300

    yoooooo

--- apiapp/views.py
+++ apiapp/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import FileResponse
-from .models import FirmwareUpdate, Device, Firmware
+from .models import FirmwareUpdate, Device
 
 class UpdateSensorDataView(APIView):
     def get(self, request, device_id, *args, **kwargs):
@@ -48,15 +48,60 @@ class UpdateSensorDataView(APIView):
             'firmware_version': firmware_update.firmware.firmware_version,
         }
 
-        # Construct the file download response
+        # Create a file response for the firmware file
         file_response = FileResponse(open(firmware_update.firmware.firmware_version_file.path, 'rb'))
 
-        # Set the Content-Disposition header to specify the filename
+        # Set the Content-Disposition header to specify the filename for download
         file_response['Content-Disposition'] = f'attachment; filename="{firmware_update.firmware.firmware_version_file.name}"'
 
-        # Return a dictionary containing both data and the file response
-        return {
+        # Return the data and the file response in a single response
+        response_data = {
             'message': 'SPV value updated successfully',
             'data': data,
             'file': file_response
         }
+
+        return response_data
+
+
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from django.http import JsonResponse
+# from .models import FirmwareUpdate
+# from django.views.decorators.csrf import csrf_exempt
+
+# class UpdateSensorDataView(APIView):
+#     def get(self, request, device_id, *args, **kwargs):
+#         try:
+#             version = request.query_params.get('version')
+#             spvValue = int(request.query_params.get('spvValue'))
+#             batteryValue = int(request.query_params.get('batteryValue'))
+#         except (ValueError, TypeError):
+#             return Response(
+#                 {
+#                 'error': 'Invalid input data'
+#                 },
+#                 status=400
+#                 )
+
+#         try:
+#             firmware_update = FirmwareUpdate.objects.get(device_id=device_id)
+#         except FirmwareUpdate.DoesNotExist:
+#             return Response(
+#                 {
+#                 'error': 'Device not found'
+#                 },
+#                 status=404
+#                 )
+#         firmware_update.version = version
+#         firmware_update.spvValue = spvValue
+#         firmware_update.batteryValue = batteryValue
+#         firmware_update.save()
+#         to_show = FirmwareUpdate.objects.filter(device_id=device_id)
+#         return Response(
+#             {
+#                 'message': 'Data updated successfully',
+#                 'data': list(to_show.values())
+#                 },
+#                 status=200
+#                 )
