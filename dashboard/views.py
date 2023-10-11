e75edbb56d311125b61982c2bacf05e757475c9c
OlukaGibson <gibsonoluka7@gmail.com>
2023-10-11 11:48:41 +0300

    new day

--- dashboard/views.py
+++ dashboard/views.py
@@ -449,68 +449,34 @@ def history(request,pk):
 
 @login_required
 def display_firmware_updates(request):
+    firmware_updates = FirmwareUpdate.objects.all()
+    fields = Fields.objects.all()
 
-    # group_by = request.GET.get('group_by')
+    # Create a list to store the data for each entry
+    firmware_update_data = []
 
-    if 1==2:
-        firmware_updates = FirmwareUpdate.objects.filter(fields__edit=False)
-        fields = Fields.objects.filter(edit=False)
+    for firmware_update in firmware_updates:
+        device_name = firmware_update.device_name.device_name
+        channel_id = firmware_update.device_name.channel_id
+        firmware_version = firmware_update.firmware.firmware_version
+        fields = firmware_update.fields.all()
+        field_data = []
 
-        # Create a list to store the data for each entry
-        firmware_update_data = []
-
-        for firmware_update in firmware_updates:
-            device_name = firmware_update.device_name.device_name
-            channel_id = firmware_update.device_name.channel_id
-            firmware_version = firmware_update.firmware.firmware_version
-            fields = firmware_update.fields.all()
-            field_data = []
-
-            for field in fields:
-                firmware_update_field = FirmwareUpdateField.objects.get(
-                    firmware_update=firmware_update, field=field)
-                field_data.append({
-                    'field_name': field.field_name,
-                    'value': firmware_update_field.value
-                })
-
-            firmware_update_data.append({
-                'device_name': device_name,
-                'channel_id': channel_id,
-                'firmware_version': firmware_version,
-                'fields': field_data
+        for field in fields:
+            firmware_update_field = FirmwareUpdateField.objects.get(
+                firmware_update=firmware_update, field=field)
+            field_data.append({
+                'field_name': field.field_name,
+                'value': firmware_update_field.value
             })
 
-    else:
-        firmware_updates = FirmwareUpdate.objects.filter(fields__edit=True)
-        fields = Fields.objects.filter(edit=True)
+        firmware_update_data.append({
+            'device_name': device_name,
+            'channel_id': channel_id,
+            'firmware_version': firmware_version,
+            'fields': field_data
+        })
 
-        # Create a list to store the data for each entry
-        firmware_update_data = []
-
-        for firmware_update in firmware_updates:
-            device_name = firmware_update.device_name.device_name
-            channel_id = firmware_update.device_name.channel_id
-            firmware_version = firmware_update.firmware.firmware_version
-            fields = firmware_update.fields.all()
-            field_data = []
-
-            for field in fields:
-                firmware_update_field = FirmwareUpdateField.objects.get(
-                    firmware_update=firmware_update, field=field)
-                field_data.append({
-                    'field_name': field.field_name,
-                    'value': firmware_update_field.value
-                })
-
-            firmware_update_data.append({
-                'device_name': device_name,
-                'channel_id': channel_id,
-                'firmware_version': firmware_version,
-                'fields': field_data
-            })
-
-    
     context = {
         'firmware_update_data': firmware_update_data,
         'fields': fields,
