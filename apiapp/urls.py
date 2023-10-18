35b5ecd0a82afe4871eb993cf407901950630f94
OlukaGibson <gibsonoluka7@gmail.com>
2023-10-18 08:57:33 +0300

    jjj

--- apiapp/urls.py
+++ apiapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UpdateSensorDataView
 
 urlpatterns = [
-    path('update-sensor-data/<str:id>', UpdateSensorDataView.as_view(), name='update-sensor-data'),
+    path('update-sensor-data/<str:id>', UpdateSensorDataView.as_view(), name='update-sensor-data'), 
 ]
