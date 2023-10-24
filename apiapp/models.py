dd64c1b5b34eab5126e5edac61ea7af1d784639a
OlukaGibson <gibsonoluka7@gmail.com>
2023-10-24 10:16:05 +0300

    file edits

--- apiapp/models.py
+++ apiapp/models.py
@@ -14,7 +14,7 @@ class Device(models.Model):
 class Firmware(models.Model):
     firmware_version = models.CharField(max_length=20, default='Firmware Version', null=True)
     firmware_version_file = models.FileField(upload_to='firmware/', null=True)
-    uploaded_at = models.DateTimeField(auto_created=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.firmware_version
