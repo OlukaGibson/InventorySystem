cfcb8a8ed5f3df806313b101708ff9fef2f5c307
OlukaGibson <gibsonoluka7@gmail.com>
2023-11-09 10:53:55 +0300

    ggg

--- Inventory/urls.py
+++ Inventory/urls.py
@@ -18,8 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
-from django.conf.urls.static import static 
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static products/ticfiles_urlpatterns
 
 #from user import views as user_view
 
