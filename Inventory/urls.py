b5debad851fbb59dc2d7feae2685077d28f5d123
OlukaGibson <gibsonoluka7@gmail.com>
2024-01-08 13:26:36 +0300

    letsgo

--- Inventory/urls.py
+++ Inventory/urls.py
@@ -20,6 +20,7 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.media import media_urlpatterns
 
 #from user import views as user_view
 
@@ -29,6 +30,6 @@ urlpatterns = [
     path('',include('user.urls')),
     path('', include('apiapp.urls')),
     path('login/', auth_views.LoginView.as_view(template_name = 'user/login.html'),name='login'),
-] + staticfiles_urlpatterns()
+] + staticfiles_urlpatterns() + media_urlpatterns()
 
 #urlpatterns +=    
