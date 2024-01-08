8b94b401ebd7a0c742bbc04f02123d46c010c62f
OlukaGibson <gibsonoluka7@gmail.com>
2024-01-08 11:08:01 +0300

    letsgo

--- Inventory/settings.py
+++ Inventory/settings.py
@@ -150,14 +150,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'assets/'
+STATIC_URL = 'static/'
 
 
-STATICFILES_DIRS = [BASE_DIR / 'assets',]
+STATICFILES_DIRS = [BASE_DIR / 'static',]
 
-# STATIC_ROOT = (BASE_DIR/"assets/")
+STATIC_ROOT = (BASE_DIR/"assets/")
 
-# MEDIA_ROOT = (BASE_DIR/'media/')
+MEDIA_ROOT = (BASE_DIR/'media/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
