16f9f4cca65e67fefc7846dab8d94b1ca5e54666
OlukaGibson <gibsonoluka7@gmail.com>
2024-01-12 14:06:26 +0300

    aws

--- apiapp/views.py
+++ apiapp/views.py
@@ -2,12 +2,36 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import FirmwareUpdate, Device, Firmware, Fields, FirmwareUpdateField, FirmwareUpdateHistory
 import json
+from django.http import HttpResponse
+import mimetypes
+import os
+
 class UpdateSensorDataView(APIView):
     def get(self, request, id, *args, **kwargs):
         try:
             device = Device.objects.get(channel_id=id)
         except Device.DoesNotExist:
             return Response({'error': 'Device not found'}, status=404)
+
+        # Check if fileDownload is 0 for the device
+        if device.fileDownload == 0:
+            firmware_update = FirmwareUpdate.objects.filter(device_name=device).first()
+
+            # Check if a firmware update is associated with the device
+            if firmware_update:
+                firmware = firmware_update.firmware
+
+                # Download the firmware_version_file
+                firmware_file_path = firmware.firmware_version_file.path
+                with open(firmware_file_path, 'rb') as firmware_file:
+                    response = HttpResponse(firmware_file.read(), content_type=mimetypes.guess_type(firmware_file_path)[0])
+                    response['Content-Disposition'] = f'attachment; filename={os.path.basename(firmware_file_path)}'
+
+                # Update the fileDownload field to 1
+                device.fileDownload = 1
+                device.save()
+
+                return response
 
         firmware_updates = FirmwareUpdate.objects.filter(device_name=device)
         firmware_update_data = []
