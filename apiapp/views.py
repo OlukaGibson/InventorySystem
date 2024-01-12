f9ed6006bcd6ec643bbdafd2f7c350b3b2fa904c
OlukaGibson <gibsonoluka7@gmail.com>
2024-01-12 17:08:03 +0300

    api

--- apiapp/views.py
+++ apiapp/views.py
@@ -6,14 +6,16 @@ import json
 
 class UpdateSensorDataView(APIView):
     def get(self, request, id, *args, **kwargs):
+        # Get the device with the channel_id
         try:
             device = Device.objects.get(channel_id=id)
         except Device.DoesNotExist:
             return Response({'error': 'Device not found'}, status=404)
 
+
+        # Get the latest firmware update for the device
         firmware_updates = FirmwareUpdate.objects.filter(device_name=device)
         firmware_update_data = []
-
         for firmware_update in firmware_updates:
             device_name = firmware_update.device_name.device_name
             channel_id = firmware_update.device_name.channel_id
@@ -22,9 +24,9 @@ class UpdateSensorDataView(APIView):
                 'device_name': device_name,
                 'channel_id': channel_id,
                 'firmware_version': firmware_version,
-                'fields': []  # Initialize an empty list for fields
+                'fields': [] 
             })
-
+            
             firmware_update_fields = FirmwareUpdateField.objects.filter(firmware_update=firmware_update)
 
             for firmware_update_field in firmware_update_fields:
